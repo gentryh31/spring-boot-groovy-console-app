9f996a80d89172beeb486985021b16140ff4c89e
Coderdood <mtrojahn@gmail.com>
2015-11-23 15:50:46 -0200

    initial upload

---
+++ src/main/groovy/org/example/Application.groovy
@@ -0,0 +1,22 @@
+package org.example
+
+import org.springframework.boot.Banner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+
+@SpringBootApplication
+class Application {
+    static main(args) {
+        def context = new SpringApplicationBuilder()
+                .sources(Application.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run()
+
+        def app = context.getBean(Application.class)
+        app.start()
+    }
+
+    def start() {
+        println("Hello World!")
+    }
+}
